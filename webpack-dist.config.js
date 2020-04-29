5ee8537a9ba6ad7652e7ab47ea4bad31250f894f
rex_yan <rex_yan@126.com>
2020-04-30 00:35:25 +0800

    up

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -102,7 +102,7 @@ module.exports = {
       // 调用 Chrome 渲染出 PDF 文件
       const chromePath = findChrome();
       spawnSync(chromePath, ['--headless', '--disable-gpu', `--print-to-pdf=${path.resolve(outputPath, 'resume.pdf')}`,
-        'https://yanrs.me/resume' // 这里注意改成你的在线简历的网站
+        'https://yanrs.me/resume/' // 这里注意改成你的在线简历的网站
       ]);
 
       // 重新发布到 ghpages
